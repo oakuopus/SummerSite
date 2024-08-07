42f69fd578a8123a1db81af3f858ca14fd427a2d
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-08-07 15:20:28 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+var scrolled = false;
 $(document).ready(function() {
     $(window).scroll(function() {
-        $(".contact").css({
-            "animation": "roll 3000ms",
+        $(".summer").css({
+            "top": "0"
         })
+        if(!scrolled){
+            $(".contact").css({
+                "animation": "roll 3000ms forwards",
+            })
+            scrolled = true;
+        }
     })
 })
