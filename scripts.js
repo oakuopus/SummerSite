d12124b96638f884bbb7909bedb297ee0b77f1f4
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-08-09 11:53:19 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-var scrolled = false;
+var scrolled = false; //set scrolled to false
 $(document).ready(function() {
+    // ensure window is loaded before running and js
     $(window).scroll(function() {
         $(".summer").css({
-            "top": "0"
+            "top": "0" // when windown is scrolled, allow summer logo to move freely
         })
-        if(!scrolled){
+        if(!scrolled){ // if scrolled is stil false, add animation to ball
             $(".contact").css({
                 "animation": "roll 3000ms forwards",
             })
-            scrolled = true;
+            scrolled = true; // set scrolled to true to ensure animation does not retrigger
         }
     })
 })
